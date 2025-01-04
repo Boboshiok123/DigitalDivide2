854e04c5945038077be3bf545d5718b7b305d0d7
Boboshiok123 <smpiggyhigh@gmail.com>
2025-01-04 21:16:03 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(screen.width* 1.6, screen.height* 1.3);
+  createCanvas(screen.width* 2, screen.height* 1.6);
   background(220);
 
   // Initialize p5.SpeechRec
